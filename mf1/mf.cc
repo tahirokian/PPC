fc4ad16d4930537d68bb050f88305b3ade7da1d7
Irshad Tahir <irshadt1@lunni.org.aalto.fi>
2016-04-15 17:17:19 +0300

    mf1 submission

--- mf1/mf.cc
+++ mf1/mf.cc
@@ -1,8 +1,42 @@
 #include "mf.h"
+#include <vector>
+#include <algorithm>    // std::max, std::min, std::nth_element
+
+inline float median(std::vector<float> &v){
+  float median;
+  std::size_t n;
+  if(v.empty()) {
+    return 0.0;
+  }
+  n = v.size() / 2;
+  std::nth_element(v.begin(), (v.begin()+n), v.end());
+  median = v[n];
+  if((v.size() % 2) == 0) {	//If even size
+    auto max_it = max_element(v.begin(), v.begin()+n);
+    median = (*max_it + median) / 2;
+  }
+  return median;
+}
 
 void mf(int ny, int nx, int hy, int hx, const float* in, float* out) {
-    // FIXME
-    for (int i = 0; i < ny * nx; ++i) {
-        out[i] = in[i];
+  int yUp, yDown, xLeft, xRight;
+  std::vector<float> v;
+  v.reserve(hx*hy);
+  for (int y = 0; y < ny; y++){
+      yUp = std::max( (y-hy), 0 );
+      yDown = std::min( (y+hy), (ny-1) );
+      for (int x = 0; x < nx; x++){
+        xLeft = std::max( (x-hx), 0 );
+        xRight = std::min( (x+hx), (nx-1) );
+        v.clear();
+        for (int j=yUp; j<=yDown; j++){
+          for (int i=xLeft; i<=xRight; i++){
+            v.push_back(in[i + (j*nx)]);
+          }
+        }
+        out[x + (y*nx)] = median(v);
+      }
     }
 }
+
+
