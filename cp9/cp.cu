53d79264209660fb0a29686468ff40debaa9ac92
Tahir Irshad <tahir.irshad@aalto.fi>
2016-05-21 14:29:22 +0300

    cp9 submission

--- cp9/cp.cu
+++ cp9/cp.cu
@@ -4,30 +4,33 @@
 #include <cmath>       // std::sqrt, std::ceil
 #include "cp.h"
 #include <cuda_runtime.h>
-#include <iostream>
+
 #define TILE 16
 
 //Kernel code
-__global__ void correlationKernel(float* input, float* output, int nx, int ny){
+__global__ void correlationKernel(float* input, float* inputTr, float* output, int nx, int ny){
   int bx = blockDim.x, by = blockDim.y;
   int tx = threadIdx.x, ty = threadIdx.y;
   int y = ty + blockIdx.y * TILE;
   int x = tx + blockIdx.x * TILE;
-  __shared__ float subInX[TILE][TILE];
+  __shared__ float subIn1[TILE][TILE];
+  __shared__ float subIn2[TILE][TILE];
   float sum = 0.0;
-  for (int i = 0; i < ((nx-1)/TILE)+1; ++i){  //rounding up --> number of tiles
-    if ((i * TILE + tx) < nx && y < ny){
-      subInX[ty][tx] = input[y*nx + i*TILE + tx];
-    }
-    else{
-      subInX[ty][tx] = 0;
-    }
+  for (int i = 0; i < ((nx-1)/TILE)+1; ++i){   //rounding up number of blocks value
+    if ((i * TILE + tx) < nx && y < ny)
+      subIn1[ty][tx] = input[y*nx + i*TILE + tx];
+    else
+      subIn1[ty][tx] = 0;
+    if ((i * TILE + ty) < nx && x < ny)
+      subIn2[ty][tx] = inputTr[(i*TILE + ty)*ny + x];
+    else
+      subIn2[ty][tx] = 0;
     __syncthreads();
     for (int j = 0; j < TILE; ++j)
-      sum += subInX[ty][j]*subInX[tx][j];
+      sum += subIn1[ty][j]*subIn2[j][tx];
     __syncthreads();
   }
-  if (y >= ny || x >= ny)
+  if (x >=ny || y >= ny || x < y)
     return;
   output[((blockIdx.y*by+ty)*ny)+(blockIdx.x*bx)+tx] = sum;
 }
@@ -38,12 +41,15 @@ void correlate(int ny, int nx, const float* data, float* result) {
   size_t inputSize = ny * nx;
   size_t outputSize = ny * ny;
   float* hostIn = 0;
+  float* hostInTr = 0;
   float* deviceIn = 0;
   float* deviceInTr = 0;
   float* deviceOut = 0;
   std::vector<float> zeroMeanVec(nx), elemSqrdVec(nx);
   cudaMallocHost((void**) &hostIn, inputSize * sizeof(float));
+  cudaMallocHost((void**) &hostInTr, inputSize * sizeof(float));
   cudaMalloc((void**) &deviceIn, inputSize * sizeof(float));
+  cudaMalloc((void**) &deviceInTr, inputSize * sizeof(float));
   cudaMalloc((void**) &deviceOut, outputSize * sizeof(float));
   dim3 blockSize(TILE,TILE);                                                          //block of 8x8
   dim3 gridSize(std::ceil(float(ny)/blockSize.x), std::ceil(float(ny)/blockSize.y));  //grid of (ny/8)x(ny/8)
@@ -63,14 +69,23 @@ void correlate(int ny, int nx, const float* data, float* result) {
     //Save the normalized result in a matrix of dimension ny*nx
     std::copy(zeroMeanVec.begin(), zeroMeanVec.end(), hostIn+rowStart);
   }
+  //Matrix transpose
+  for (int j=0; j<ny; ++j){
+    for (int i=0; i<nx; ++i){
+      hostInTr[i*ny+j] = hostIn[j*nx+i];
+    }
+  }
   //Copy host data to GPU
   cudaMemcpy(deviceIn, hostIn, inputSize * sizeof(float), cudaMemcpyHostToDevice);
+  //Copy hostTranspose data to GPU
+  cudaMemcpy(deviceInTr, hostInTr, inputSize * sizeof(float), cudaMemcpyHostToDevice);
   //Kernel call
-  correlationKernel<<<gridSize, blockSize>>>(deviceIn, deviceOut, nx, ny);
+  correlationKernel<<<gridSize, blockSize>>>(deviceIn, deviceInTr, deviceOut, nx, ny);
   //Copy GPU data to host
   cudaMemcpy(result, deviceOut, outputSize * sizeof(float), cudaMemcpyDeviceToHost);
   //Free memory
   cudaFreeHost(hostIn);
+  cudaFreeHost(hostInTr);
   cudaFree(deviceIn);
   cudaFree(deviceInTr);
   cudaFree(deviceOut);
